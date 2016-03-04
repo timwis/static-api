285b68f6d2fd37e139a0526f5f04648a1e73d39b
timwis <tim@timwis.com>
2016-03-04 06:28:34 -0500

    use double quotes instead of single quotes

--- build.rb
+++ build.rb
@@ -1,25 +1,25 @@
-require 'fileutils'
-require 'csv'
-require 'json'
-require 'slugify'
+require "fileutils"
+require "csv"
+require "json"
+require "slugify"
 
-BUILD_DIR = 'dist'
+BUILD_DIR = "dist"
 FileUtils.rm_rf(BUILD_DIR) if Dir.exist?(BUILD_DIR)
 Dir.mkdir(BUILD_DIR)
 
-primary_key = ENV['PRIMARY_KEY'] || 'id'
+primary_key = ENV["PRIMARY_KEY"] || "id"
 
-csv_files = Dir['./*.csv'].reject { |f| File.directory?(f) }
+csv_files = Dir["./*.csv"].reject { |f| File.directory?(f) }
 
 csv_files.each do |file_name|
-  dataset_slug = File.basename(file_name, '.csv').slugify
+  dataset_slug = File.basename(file_name, ".csv").slugify
   Dir.mkdir("#{BUILD_DIR}/#{dataset_slug}") unless Dir.exist?("#{BUILD_DIR}/#{dataset_slug}")
 
   rows = Array.new
 
   # Write a file for each row
   CSV.foreach(file_name, {
-    encoding: 'UTF-8',
+    encoding: "UTF-8",
     headers: true,
     header_converters: :symbol,
     converters: :all
@@ -27,10 +27,10 @@ csv_files.each do |file_name|
     rows.push(row.to_hash)
     target_file = row[primary_key.to_sym].to_s.slugify
     row_json = JSON.generate(row.to_hash)
-    File.open("#{BUILD_DIR}/#{dataset_slug}/#{target_file}.json", 'w') { |f| f.write(row_json) }
+    File.open("#{BUILD_DIR}/#{dataset_slug}/#{target_file}.json", "w") { |f| f.write(row_json) }
   end
 
   # Write full dataset to file
   rows_json = JSON.generate(rows)
-  File.open("#{BUILD_DIR}/#{dataset_slug}.json", 'w') { |f| f.write(rows_json) }
+  File.open("#{BUILD_DIR}/#{dataset_slug}.json", "w") { |f| f.write(rows_json) }
 end
